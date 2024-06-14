77595aa480220fa46d8c456a5f8ae5a6e8cf55ca
Den Roko <hpmsolutions.hq@gmail.com>
2024-06-14 21:39:21 +0800

    push3

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { FaBeer, FaCoffee, FaApple, FaAndroid } from 'react-icons/fa';
+import axios from 'axios';
 import './Home.css'; // Import the CSS file
 
 const Home = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://rfidboomgate.vercel.app/api/data');
+      setData(response.data);
+      setError(null);
+    } catch (error) {
+      setError('Error fetching data');
+      setData(null);
+    }
+  };
+
   return (
-    <Container className="custom-container" class="bgcolor-warning">
+    <Container className="custom-container bgcolor-warning">
       <Row className="custom-row">
         <Col xs={12} md={3}>
           <Card className="text-center">
             <Card.Body className="icon-hover">
               <FaBeer size={50} />
-              <Card.Title>Beerr</Card.Title>
+              <Card.Title>Beer</Card.Title>
             </Card.Body>
           </Card>
         </Col>
@@ -40,22 +55,27 @@ const Home = () => {
           </Card>
         </Col>
       </Row>
+      <Row className="custom-row">
+        <Col xs={12} md={3}>
+          <Button onClick={fetchData}>Fetch Data</Button>
+        </Col>
+      </Row>
+      {error && (
+        <Row className="custom-row">
+          <Col>
+            <p>{error}</p>
+          </Col>
+        </Row>
+      )}
+      {data && (
+        <Row className="custom-row">
+          <Col>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
 
 export default Home;
-
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     try {
-//       const response = await axios.get('https://rfidboomgate.vercel.app/api/data/');
-//       setData(response.data);
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
-
-//   fetchData();
-// }, []);
